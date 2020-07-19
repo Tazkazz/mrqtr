9e4e2cf3181d1e0d52caa6d2a9243a113d2f9e07
Lukas Taškūnas <lukas@inventi.lt>
2020-07-19 12:27:21 +0300

    init: add Spring Boot boilerplate

---
+++ src/main/java/lt/tazkazz/mrqtr/MusicLibraryApplication.java
@@ -0,0 +1,11 @@
+package lt.tazkazz.mrqtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicLibraryApplication.class, args);
+    }
+}
---
+++ src/test/java/lt/tazkazz/mrqtr/MusicLibraryApplicationTest.java
@@ -0,0 +1,13 @@
+package lt.tazkazz.mrqtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicLibraryApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
